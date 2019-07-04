86ef322c1de111b773d0856d40b03883d7dc3b08
Николай <nikolai.kotikov@gmail.com>
2019-07-04 22:43:57 +0300

    устранил замечания к заданию №13

--- js/backend.js
+++ js/backend.js
@@ -3,43 +3,51 @@
 (function () {
   var URL = 'https://js.dump.academy/code-and-magick/data';
 
-  window.backend = {
-    load: function (onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
+  var createRequest = function () {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    return xhr;
+  };
 
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения :( Срочно перезагрузите страницу, пока не случилось что-то похуже!');
-      });
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
+  window.load = function (onLoad, onError) {
+    var xhr = createRequest();
 
-      xhr.timeout = 10000; // 10s
-
-      xhr.open('GET', URL);
-      xhr.send();
-    },
-    save: function (data, onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
         onLoad(xhr.response);
-        xhr.addEventListener('error', function () {
-          onError('Не удалось отправить данные');
-        });
-      });
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения :( Срочно перезагрузите страницу, пока не случилось что-то похуже!');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
 
-      xhr.open('POST', URL);
-      xhr.send(data);
-    }
+    xhr.timeout = 10000; // 10s
+
+    xhr.open('GET', URL);
+    xhr.send();
+  };
+
+  window.save = function (data, onLoad, onError) {
+    var xhr = createRequest();
+
+    xhr.addEventListener('load', function () {
+      onLoad(xhr.response);
+      xhr.addEventListener('error', function () {
+        onError('Не удалось отправить данные');
+      });
+    });
+
+    xhr.open('POST', URL);
+    xhr.send(data);
+  };
+
+  window.backend = {
+    load: window.load,
+    save: window.save
   };
 })();
